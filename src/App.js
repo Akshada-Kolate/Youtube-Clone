38384183020769f72ebe1adbc2447f111ac857a6
Akshada Kolate <90676596+Akshada-Kolate@users.noreply.github.com>
2023-01-20 21:47:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Box from '@mui/material/Box';
+
+import {Navbar , Feed , VedioDetail , ChannelDetail , SearchFeed } from './components'
+
+
+
+const App = () => (
+    <BrowserRouter>
+     <Box sx={{ backgroundColor: '#000'}}>
+      <Navbar />
+     <Routes>
+        <Route path="/" exact element={<Feed />} />
+          <Route path="/vedio/:id" element={<VedioDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+     </Box>
+    </BrowserRouter>
+  )
+
+
+export default App
